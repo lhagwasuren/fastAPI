dcabef515c1574a45c424cae65101949ad60dc3e
lhagwasuren <lkhagvasureen12@gmail.com>
2021-12-22 22:23:09 +0800

    first add github action

--- app/database.py
+++ app/database.py
@@ -2,9 +2,6 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import time
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
--- app/routers/post.py
+++ app/routers/post.py
@@ -57,21 +57,23 @@ def create_post(post: schemas.PostCreate, db:Session = Depends(get_db),
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int, db:Session = Depends(get_db), 
     current_user :int = Depends(oauth2.get_current_user)):
-    # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # conn.commit()
-    # deleted_post = cursor.fetchone()
+    
     post_query = db.query(models.Post).filter(models.Post.id == id)
+
     post = post_query.first()
 
     if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with {id} does not exist")
-    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
     if post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform request action")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
 
     post_query.delete(synchronize_session=False)
     db.commit()
-    return Response(status_code= status.HTTP_204_NO_CONTENT)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put('/{id}', response_model=schemas.Post)
 def update_post(id:int, update_post: schemas.PostCreate, db:Session = Depends(get_db), 
